c786582522aa05c480918f032209f4655a979464
Jakub Černý <AsKronak@seznam.cz>
2025-03-23 16:36:18 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+// Add custom JavaScript here
+function userScroll() {
+  const navbar = document.querySelector(".navbar");
+  const toTopBtn = document.querySelector(".to-top-btn");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+      navbar.classList.add("navbar-sticky");
+      toTopBtn.classList.add("show");
+    } else {
+      navbar.classList.remove("navbar-sticky");
+      toTopBtn.classList.remove("show");
+    }
+  });
+}
+
+function scrollUp() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+function icrementStats() {
+  const counters = document.querySelectorAll(".counter");
+
+  counters.forEach((counter) => {
+    counter.innerText = 0;
+    const updateCounter = () => {
+      const target = +counter.getAttribute("data-target");
+      const c = +counter.innerText;
+
+      const increment = target / 1000;
+
+      if (c < target) {
+        counter.innerText = Math.ceil(c + increment);
+        setTimeout(updateCounter, 1);
+      } else {
+        counter.innerText = target;
+      }
+    };
+    updateCounter();
+  });
+}
+
+//EVENT LISTENERS
+document.addEventListener("DOMContentLoaded", userScroll);
+document.addEventListener("DOMContentLoaded", icrementStats);
+document.querySelector(".to-top-btn").addEventListener("click", scrollUp);
